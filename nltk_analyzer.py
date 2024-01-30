6634c20cf9519fb0ce3ae6ea9cd5211e178e0528
AA <121849896+lixavi@users.noreply.github.com>
2024-01-30 23:15:50 +0530

    Update nltk_analyzer.py

--- nltk_analyzer.py
+++ nltk_analyzer.py
@@ -1,6 +1,20 @@
 import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 class NLTKAnalyzer:
+    def __init__(self):
+        self.sid = SentimentIntensityAnalyzer()
+
     def analyze_sentiment(self, text):
         # NLTK sentiment analysis implementation
-        pass
+        scores = self.sid.polarity_scores(text)
+        compound_score = scores['compound']
+        
+        if compound_score >= 0.05:
+            sentiment = 'positive'
+        elif compound_score <= -0.05:
+            sentiment = 'negative'
+        else:
+            sentiment = 'neutral'
+        
+        return sentiment
