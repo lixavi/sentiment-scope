b0e0458bc2d3ad44ec6b8dc5668432388685fa52
AA <121849896+lixavi@users.noreply.github.com>
2024-01-30 23:16:27 +0530

    Updated

--- nltk_analyzer.py
+++ nltk_analyzer.py
@@ -1,20 +1,36 @@
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tag import pos_tag
+from nltk.chunk import ne_chunk
 
 class NLTKAnalyzer:
     def __init__(self):
         self.sid = SentimentIntensityAnalyzer()
+        nltk.download('punkt')
+        nltk.download('averaged_perceptron_tagger')
+        nltk.download('maxent_ne_chunker')
+        nltk.download('words')
 
     def analyze_sentiment(self, text):
         # NLTK sentiment analysis implementation
-        scores = self.sid.polarity_scores(text)
-        compound_score = scores['compound']
+        sentences = sent_tokenize(text)
+        tokenized_sentences = [word_tokenize(sentence) for sentence in sentences]
+        pos_tagged_sentences = [pos_tag(tokens) for tokens in tokenized_sentences]
+        named_entities = [ne_chunk(pos_tags) for pos_tags in pos_tagged_sentences]
+
+        compound_scores = []
+        for sentence in tokenized_sentences:
+            scores = self.sid.polarity_scores(" ".join(sentence))
+            compound_scores.append(scores['compound'])
+
+        overall_score = sum(compound_scores) / len(compound_scores)
         
-        if compound_score >= 0.05:
+        if overall_score >= 0.05:
             sentiment = 'positive'
-        elif compound_score <= -0.05:
+        elif overall_score <= -0.05:
             sentiment = 'negative'
         else:
             sentiment = 'neutral'
-        
-        return sentiment
+
+        return sentiment, named_entities
